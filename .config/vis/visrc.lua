0df87d28c01a35fe9629c65ab4450917c4506f3e
Will Green <72352225+willgreen946@users.noreply.github.com>
2024-01-15 20:23:01 +0000

    Add files via upload

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -5,7 +5,7 @@ end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
 	vis:command('set number')
-	vis:command('set theme minimal-dark')
+	vis:command('set theme red')
 	vis:command('set escdelay 0')
 	vis:command('set tabwidth 2')
 	vis:command('set colorcolumn 80')
