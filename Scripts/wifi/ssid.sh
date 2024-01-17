c31dc72576e11d00bae1c05574c48a68b47839a4
Will Green <72352225+willgreen946@users.noreply.github.com>
2024-01-17 21:23:04 +0000

    Linux support

--- Scripts/wifi/ssid.sh
+++ Scripts/wifi/ssid.sh
@@ -1,3 +1,9 @@
+#
 # List all available wireless networks
 # Probably not the most efficient way but it works
-ifconfig $1 scan | grep nwid | awk '{print $2}' | grep -v '^"' | grep -v '^0'
+#
+if [ "${uname}" -eq "OpenBSD" ]; then
+	ifconfig $1 scan | grep nwid | awk '{print $2}' | grep -v '^"' | grep -v '^0'
+else
+	iw $1 scan | grep SSID
+fi
