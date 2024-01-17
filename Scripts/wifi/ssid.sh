df60b67506351dd78fb04769cd0980f91757196d
Will Green <72352225+willgreen946@users.noreply.github.com>
2024-01-17 21:01:08 +0000

    Only show real networks

--- Scripts/wifi/ssid.sh
+++ Scripts/wifi/ssid.sh
@@ -1,4 +1,3 @@
 # List all available wireless networks
-dhclient $1
-sleep 1
-ifconfig $1 scan | grep nwid | awk '{print $2}'
+# Probably not the most efficient way but it works
+ifconfig $1 scan | grep nwid | awk '{print $2}' | grep -v '^"' | grep -v '^0'
