0bf02933549506de38c874de3392293312090554
Will Green <72352225+willgreen946@users.noreply.github.com>
2024-03-30 18:31:04 +0000

    Add files via upload

--- Scripts/openbsd/wifi/eduroam.sh
+++ Scripts/openbsd/wifi/eduroam.sh
@@ -1,1 +1,4 @@
-
+dhclient $1 
+ifconfig $1 -wpakey
+ifconfig $1 nwid eduroam wpa wpaakms 802.1x up
+exec wpa_supplicant -i $1 -D openbsd -c ~/Scripts/wifi/configs/eduroam_chesterfield_college.conf -B
---
+++ Scripts/openbsd/wifi/home.sh
@@ -0,0 +1,2 @@
+dhclient $1
+ifconfig $1 nwid "BT-J5A85S" wpakey "c7G7dbLQK7APhE"
