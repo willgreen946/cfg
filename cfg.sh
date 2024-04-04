c4b406b2e5f37ec88545e081f4a44466b318d01f
Will Green <72352225+willgreen946@users.noreply.github.com>
2024-04-04 10:35:43 +0100

    Add files via upload

---
+++ cfg.sh
@@ -0,0 +1,17 @@
+cp -r .fvwm ~/
+cp -r .fonts ~/
+cp -r .links ~/
+cp -r .config ~/
+cp -r Scripts ~/
+cp -r Pictures ~/
+cp .shrc ~/
+cp .nexrc ~/
+cp .cshrc ~/
+cp .twmrc ~/
+cp .vimrc ~/
+cp .bashrc ~/
+cp .ctwmrc ~/
+cp .lynxrc ~/
+cp .xinitrc ~/
+cp .tmux.conf ~/
+cp .Xdefaults ~/
