8c7027c8d6d812acac8c97649d554836cd0ce36a
Will Green <72352225+willgreen946@users.noreply.github.com>
2024-04-05 20:54:52 +0100

    Update cfg.sh

--- cfg.sh
+++ cfg.sh
@@ -1,17 +1,20 @@
-cp -r .fvwm ~/
-cp -r .fonts ~/
-cp -r .links ~/
-cp -r .config ~/
-cp -r Scripts ~/
-cp -r Pictures ~/
-cp .shrc ~/
-cp .nexrc ~/
-cp .cshrc ~/
-cp .twmrc ~/
-cp .vimrc ~/
-cp .bashrc ~/
-cp .ctwmrc ~/
-cp .lynxrc ~/
-cp .xinitrc ~/
-cp .tmux.conf ~/
-cp .Xdefaults ~/
+cp -rv .fvwm ~/
+cp -rv .fonts ~/
+cp -rv .links ~/
+cp -rv .config ~/
+cp -rv Scripts ~/
+cp -rv Pictures ~/
+mkdir -p ~/Documents
+mkdir -p ~/Downloads
+mkdir -p ~/Source
+cp -v .shrc ~/
+cp -v .nexrc ~/
+cp -v .cshrc ~/
+cp -v .twmrc ~/
+cp -v .vimrc ~/
+cp -v .bashrc ~/
+cp -v .ctwmrc ~/
+cp -v .lynxrc ~/
+cp -v .xinitrc ~/
+cp -v.tmux.conf ~/
+cp -v .Xdefaults ~/
