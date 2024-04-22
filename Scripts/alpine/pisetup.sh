386063b182f681bf3fe2539ad2e1489b5b3b1eb7
Will Green <72352225+willgreen946@users.noreply.github.com>
2024-04-22 20:18:24 +0100

    Add files via upload

--- Scripts/alpine/pisetup.sh
+++ Scripts/alpine/pisetup.sh
@@ -1,1 +1,7 @@
+# Not a raspberry pi setup!
+# This is a post install script
+apk update
+setup-xorg-base
+apk add git firefox wpa_supplicant xterm fvwm lm-sensors htop lsblk xf86-video-intel vifm acpi mandoc man-pages mandoc-apropos cfdisk gcc g++ clang shadow oksh libreoffice-base libreoffice mpv zzz make libx11 tmux terminus-font alsa-utils alpine-sdk zip unzip sxiv zathura
 
+# chsh -s /bin/oksh will
