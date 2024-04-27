36dbb1c8156ce31e8b383500b7f09d6575215659
Will Green <72352225+willgreen946@users.noreply.github.com>
2024-04-27 22:00:55 +0100

    Add files via upload

--- Scripts/alpine/pisetup.sh
+++ Scripts/alpine/pisetup.sh
@@ -2,6 +2,6 @@
 # This is a post install script
 apk update
 setup-xorg-base
-apk add git firefox wpa_supplicant xterm fvwm lm-sensors htop lsblk xf86-video-intel vifm acpi mandoc man-pages mandoc-apropos cfdisk gcc g++ clang shadow oksh libreoffice-base libreoffice mpv zzz make libx11 tmux terminus-font alsa-utils alpine-sdk zip unzip sxiv zathura
+apk add git firefox wpa_supplicant xterm fvwm lm-sensors htop lsblk xf86-video-intel vifm acpi mandoc man-pages mandoc-apropos cfdisk gcc g++ clang shadow oksh libreoffice-base libreoffice mpv zzz make libx11 tmux terminus-font alsa-utils alpine-sdk zip unzip sxiv zathura zathura-pdf-mupdf autoconf font-liberation automake yt-dlp transmission-cli
 
 # chsh -s /bin/oksh will
