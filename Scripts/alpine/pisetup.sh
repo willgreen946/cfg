956b44a5cba830f859b318597198a2bf0ed69092
Will Green <72352225+willgreen946@users.noreply.github.com>
2024-06-02 22:20:35 +0100

    Add files via upload

--- Scripts/alpine/pisetup.sh
+++ Scripts/alpine/pisetup.sh
@@ -3,7 +3,7 @@ apk update
 setup-xorg-base
 
 # Install all the packages I need
-apk add git firefox wpa_supplicant xterm fvwm lm-sensors htop lsblk xf86-video-intel vifm acpi mandoc man-pages mandoc-apropos cfdisk gcc g++ clang shadow oksh libreoffice-base libreoffice mpv zzz make libx11 tmux terminus-font alsa-utils alpine-sdk zip unzip sxiv zathura setxkbmap groff zzz pcc pcc-doc pcc-libs objfw zathura-pdf-mupdf perl ffmpeg mpv ncurses ncurses-dev ncurses-static libncursesw ncurses-doc docs font-liberation autoconf make automake yt-dlp transmission-cli emacs-x11 python3 nerd-fonts nerd-fonts-all
+apk add git firefox wpa_supplicant xterm fvwm lm-sensors htop lsblk xf86-video-intel vifm acpi mandoc man-pages mandoc-apropos cfdisk gcc g++ clang shadow oksh libreoffice-base libreoffice mpv zzz make libx11 tmux terminus-font alsa-utils alpine-sdk zip unzip sxiv zathura setxkbmap groff zzz pcc pcc-doc pcc-libs objfw zathura-pdf-mupdf perl ffmpeg mpv ncurses ncurses-dev ncurses-static libncursesw ncurses-doc docs autoconf make automake yt-dlp transmission-cli
 
 # This is needed to get Xorg to work under a normal user account
 addgroup will video
@@ -11,3 +11,6 @@ addgroup will input
 
 # I want to use ksh as my default shell
 chsh -s /bin/oksh will
+
+# Install vim-airline
+git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline
